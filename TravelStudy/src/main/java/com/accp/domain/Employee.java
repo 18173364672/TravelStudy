99f2b93d721c5685127b5dd09d94c9f23eb8ae5f
humingfei <470759662@qq.com>
2019-04-15 17:00:47 +0800

    2019-4-15 下午

--- TravelStudy/src/main/java/com/accp/domain/Employee.java
+++ TravelStudy/src/main/java/com/accp/domain/Employee.java
@@ -3,6 +3,8 @@ package com.accp.domain;
 import java.util.Date;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class Employee {
     private Integer id;
 
@@ -17,7 +19,9 @@ public class Employee {
     private String phone;
 
     private String address;
+   
 
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
     private Date createtime;
 
     private String job;
